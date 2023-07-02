d8b522b8c842250d53130f536257bc1c2c5dd9e3
weiqixu05 <weiqixu2005@icloud.com>
2023-07-02 00:21:20 -0400

    made changes to about

--- src/frontend/pages/About/About.jsx
+++ src/frontend/pages/About/About.jsx
@@ -4,9 +4,16 @@ import "./About.css";
 export default function About(){
     return(
         <div>
-            <h1 className="head">
-                About the recipes
-            </h1>
+            <div className="top">
+                <h1>
+                    About
+                </h1>
+            </div>
+            <div className="body">
+                <p>
+                    A place for your mothers recipes. As you leave home, you will want to keep them with you, and here is the perfect place to store them.
+                </p>
+            </div>
         </div>
     )
 }
