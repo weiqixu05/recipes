7ca1318f2380a9a60a1c76c98aa57c5b0a5d5bcc
weiqixu05 <weiqixu2005@icloud.com>
2023-07-04 23:55:40 -0400

    connected edamam api

--- src/frontend/pages/Search/Search.jsx
+++ src/frontend/pages/Search/Search.jsx
@@ -3,6 +3,16 @@ import "./Search.css";
 import "../../styles.css";
 
 export default function Search(){
+    async function fetchData(){
+        try{
+            const data=((await fetch("https://api.edamam.com/api/recipes/v2?type=public&app_id=33eef802&app_key=8ebc6ae94cc66037076a9fd9c7e9c5fa")));
+            console.log(data);
+            console.log(await data.json());
+        } catch (error){
+            console.log(error);
+        }
+    }
+    fetchData();
     return(
         <h1 className="h1">If you ran out of recipes....</h1>
     )
