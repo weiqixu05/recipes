c2a6c793d879b0d63d6d36300609ff6b609a0fb1
weiqixu05 <weiqixu2005@icloud.com>
2023-07-05 23:54:29 -0400

    found link to recipe

--- src/frontend/pages/Search/Search.jsx
+++ src/frontend/pages/Search/Search.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import "./Search.css";
 import "../../styles.css";
 
+var recipeLink=2;
+
 export default function Search(){
     async function fetchData(){
         try{
-            const data=((await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=33eef802&app_key=8ebc6ae94cc66037076a9fd9c7e9c5fa")));
-            console.log(data);
-            const response=await data.json();
-            console.log(response);
+            const response=((await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=33eef802&app_key=8ebc6ae94cc66037076a9fd9c7e9c5fa")));
+            //console.log(response);
+            const data=await response.json();
+            //console.log(data);
+            recipeLink=(data.hits[0].recipe.url);
+            //console.log(jsonData.count);
         } catch (error){
             console.log(error);
         }
     }
     fetchData();
     return(
-        <h1 className="h1">If you ran out of recipes....</h1>
+        <div>
+            <h1 className="h1">If you ran out of recipes....</h1>
+            <p>
+                {recipeLink}
+            </p>
+        </div>
     )
 }
