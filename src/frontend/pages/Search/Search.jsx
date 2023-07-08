c6567307c03647cfb2409d17a66a997dd45fc8b3
weiqixu05 <weiqixu2005@icloud.com>
2023-07-08 00:52:14 -0400

    added button to change recipe link

--- src/frontend/pages/Search/Search.jsx
+++ src/frontend/pages/Search/Search.jsx
@@ -3,27 +3,34 @@ import "./Search.css";
 import "../../styles.css";
 import {useState} from "react";
 
+
+var query="pizza";
 export default function Search(){
     const [recipeLink, setRecipeLink]=useState("");
-    async function fetchData(){
+    async function fetchData(query){
         try{
-            const response=((await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=33eef802&app_key=8ebc6ae94cc66037076a9fd9c7e9c5fa")));
-            //console.log(response);
+            const response=((await fetch("https://api.edamam.com/api/recipes/v2?type=public&q="+query+"&app_id=33eef802&app_key=8ebc6ae94cc66037076a9fd9c7e9c5fa")));
             const data=await response.json();
-            //console.log(data);
             setRecipeLink(data.hits[0].recipe.url);
-            //console.log(jsonData.count);
+            console.log(recipeLink);
         } catch (error){
             console.log(error);
         } 
     }
-    fetchData();
+    fetchData(query);
     return(
         <div>
             <h1 className="h1">If you ran out of recipes....</h1>
             <p>
                 {recipeLink}
             </p>
+                <button onClick={() =>{
+                        query="dumplings";
+                        fetchData(query);
+                    }}
+                >
+                    Change recipe
+                </button>
         </div>
     )
 }
