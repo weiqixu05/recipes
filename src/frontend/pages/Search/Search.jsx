1c1b2fa089d316afd5cab61bb1bd3141b68dc8e1
weiqixu05 <weiqixu2005@icloud.com>
2023-07-07 23:38:55 -0400

    added useState to display recipe link

--- src/frontend/pages/Search/Search.jsx
+++ src/frontend/pages/Search/Search.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Search.css";
 import "../../styles.css";
-
-var recipeLink=2;
+import {useState} from "react";
 
 export default function Search(){
+    const [recipeLink, setRecipeLink]=useState("");
     async function fetchData(){
         try{
             const response=((await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=33eef802&app_key=8ebc6ae94cc66037076a9fd9c7e9c5fa")));
             //console.log(response);
             const data=await response.json();
             //console.log(data);
-            recipeLink=(data.hits[0].recipe.url);
+            setRecipeLink(data.hits[0].recipe.url);
             //console.log(jsonData.count);
         } catch (error){
             console.log(error);
@@ -22,7 +22,7 @@ export default function Search(){
         <div>
             <h1 className="h1">If you ran out of recipes....</h1>
             <p>
-                {}
+                {recipeLink}
             </p>
         </div>
     )
