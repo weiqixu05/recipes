04a99f97e7eb464a138d14e0133c54d2850eac15
weiqixu05 <weiqixu2005@icloud.com>
2023-07-09 01:10:44 -0400

    fixed recipeLink logic

--- src/frontend/pages/Search/Search.jsx
+++ src/frontend/pages/Search/Search.jsx
@@ -5,19 +5,30 @@ import {useState} from "react";
 
 
 var query="pizza";
+var hitNumber=0;
+var max=0;
+var count=0;
 export default function Search(){
     const [recipeLink, setRecipeLink]=useState("");
-    async function fetchData(query){
+    async function fetchData(query, hitNumber){
         try{
             const response=((await fetch("https://api.edamam.com/api/recipes/v2?type=public&q="+query+"&app_id=33eef802&app_key=8ebc6ae94cc66037076a9fd9c7e9c5fa")));
             const data=await response.json();
-            setRecipeLink(data.hits[0].recipe.url);
-            console.log(recipeLink);
+            console.log(hitNumber);
+            if((hitNumber>data.count-1)&&data.count!=0){
+                hitNumber=data.count-1;
+            } else if(data.count==0){
+                setRecipeLink("No Recipes Found");
+            }
+            else{setRecipeLink(data.hits[hitNumber].recipe.url);}
+            //console.log(hitNumber);
+            //console.log(data.count);
+            //console.log(recipeLink);
         } catch (error){
             console.log(error);
         } 
     }
-    fetchData(query);
+    fetchData(query, hitNumber);
     return(
         <div>
             <h1 className="h1">If you ran out of recipes....</h1>
@@ -26,10 +37,20 @@ export default function Search(){
             </p>
                 <button onClick={() =>{
                         query="dumplings";
-                        fetchData(query);
+                        fetchData(query, hitNumber);
                     }}
                 >
                     Change recipe
+                </button>
+                <button onClick={()=>{
+                        query="";
+                        max=20;
+                        hitNumber=Math.floor(Math.random()*max);
+                        //console.log(hitNumber);
+                        fetchData(query, hitNumber);
+                    }}
+                >
+                    Random Recipe
                 </button>
         </div>
     )
