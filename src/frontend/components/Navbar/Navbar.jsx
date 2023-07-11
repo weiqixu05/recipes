516587a9ba42b3440d6f2a0577b56870d97da745
weiqixu05 <weiqixu2005@icloud.com>
2023-07-11 16:40:02 -0400

    added mobile navbar

--- src/frontend/components/Navbar/Navbar.jsx
+++ src/frontend/components/Navbar/Navbar.jsx
@@ -1,19 +1,19 @@
 import "./Navbar.css";
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
+import {isMobile, isTablet, isDesktop} from "react-device-detect";
 
 export default function Navbar () {
-    const [isMobile, setIsMobile]=useState(false);
     return(
         <nav className={isMobile?"nav-mobile":"navbar"}>
-            <ul className="nav-list">
-                <Link to="/about" className="nav-item">
+            <ul className={isMobile?"nav-list-mobile":"nav-list"}>
+                <Link to="/about" className={isMobile?"nav-item-mobile":"nav-item"}>
                     <li>About</li>
                 </Link>
-                <Link to="/collection" className="nav-item">
+                <Link to="/collection" className={isMobile?"nav-item-mobile":"nav-item"}>
                     <li>Collection</li>
                 </Link>
-                <Link to="/search" className="nav-item">
+                <Link to="/search" className={isMobile?"nav-item-mobile":"nav-item"}>
                     <li>Search</li>
                 </Link>
             </ul>
