cf2be0ce633f0b46d368db11dce3e3a30526092a
weiqixu05 <weiqixu2005@icloud.com>
2023-07-12 21:18:31 -0400

    made mobile navbar

--- src/frontend/components/Navbar/Navbar.jsx
+++ src/frontend/components/Navbar/Navbar.jsx
@@ -4,7 +4,10 @@ import {Link} from "react-router-dom";
 import {isMobile, isTablet, isDesktop} from "react-device-detect";
 
 export default function Navbar () {
+    const [isPressed, setIsPressed]=useState(false);
     return(
+        <div>
+        {((isMobile&&isPressed)||(isDesktop))?(
         <nav className={isMobile?"nav-mobile":"navbar"}>
             <ul className={isMobile?"nav-list-mobile":"nav-list"}>
                 <Link to="/about" className={isMobile?"nav-item-mobile":"nav-item"}>
@@ -17,6 +20,17 @@ export default function Navbar () {
                     <li>Search</li>
                 </Link>
             </ul>
-        </nav>
+        </nav>):(<div></div>)
+    }   {isMobile?
+        (<div className="nav-mobile-header">
+            <div className="nav-button">
+                <button onClick={()=>{setIsPressed(!isPressed);}}>
+                    {(isPressed?<div>X</div>:<div>☰</div>)}
+                </button>
+            </div>
+        </div>)
+            :(<div></div>)
+        } 
+        </div>
     )
 }
