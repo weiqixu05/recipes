8227ae838328eb0697e7f2fd358efbade50545df
weiqixu05 <weiqixu2005@icloud.com>
2023-07-13 23:57:45 -0400

    preliminary layout for collection page

--- src/frontend/pages/Collection/Collection.jsx
+++ src/frontend/pages/Collection/Collection.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Collection.css";
-import "../../styles.css"
+import "../../styles.css";
 
 export default function Collection(){
+    const [imagePos, setImagePos]=useState('r');
     return(
         <div>
             <div className="head">
                 <h1>List of Recipes</h1>
+            </div>
+            <div className="recipe-body">
+                <div className={imagePos=='r'?"recipe-text":"recipe-image"}>
+                    {imagePos=='r'?setImagePos=='l':setImagePos=='r'}
+                    <p>hello world</p>
+                </div>
+                <div className={imagePos=='r'?"recipe-image":"recipe-text"}>
+                    <p>goodbye world</p>
+                </div>
             </div>
         </div>
     )
