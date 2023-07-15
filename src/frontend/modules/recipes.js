c7b92e7194599c1d1176404b4426f35b98ed7532
weiqixu05 <weiqixu2005@icloud.com>
2023-07-14 23:49:11 -0400

    iterate over recipes

---
+++ src/frontend/modules/recipes.js
@@ -0,0 +1,44 @@
+const recipeData=[
+    {
+        //img= whatever imported
+        name:"dumplings",
+        description:"chinese food",
+        ingredients:["meat", "vegetables", "dough"],
+        nutrition: [
+            {
+                calories:"100",
+                carbs:"5g",
+                protein:"1g"
+            }
+        ],
+        steps:[
+            {
+                1:"something",
+                2:"something",
+                3:"something"
+            }
+        ]
+    },
+    {
+        //img= whatever imported
+        name:"burger",
+        description:"american food",
+        ingredients:["meat", "vegetables", "bun", "condiments"],
+        nutrition: [
+            {
+                calories:"1000",
+                carbs:"50g",
+                protein:"10g"
+            }
+        ],
+        steps:[
+            {
+                1:"step",
+                2:"step",
+                3:"step"
+            }
+        ]
+    }
+]
+
+export {recipeData};
--- src/frontend/pages/Collection/Collection.jsx
+++ src/frontend/pages/Collection/Collection.jsx
@@ -1,23 +1,33 @@
 import React, {useState} from "react";
 import "./Collection.css";
 import "../../styles.css";
+import {recipeData} from "../../modules/recipes.js";
 
 export default function Collection(){
     const [imagePos, setImagePos]=useState('r');
+    const something=recipeData[0].name;
+    const[i,setI]=useState(true);
     return(
         <div>
             <div className="head">
                 <h1>List of Recipes</h1>
+                <h2>{something}</h2>
             </div>
-            <div className="recipe-body">
-                <div className={imagePos=='r'?"recipe-text":"recipe-image"}>
-                    {imagePos=='r'?setImagePos=='l':setImagePos=='r'}
-                    <p>hello world</p>
-                </div>
-                <div className={imagePos=='r'?"recipe-image":"recipe-text"}>
-                    <p>goodbye world</p>
-                </div>
-            </div>
+            {
+                [...recipeData].map(()=>{
+                    return(
+                    <div className="recipe-body">
+                        <div className={i==true?"recipe-text":"recipe-image"}>
+                            <p>hello world</p>
+                        </div>
+                        <div className={i==true?"recipe-image":"recipe-text"}>
+                            <p>goodbye world</p>
+                        </div>
+                    </div>
+                    )
+                    }
+                )
+            }
         </div>
     )
 }
