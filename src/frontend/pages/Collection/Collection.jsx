a3d12a7b8789ccbfbeeea361dbbc7ca4e0c5a437
weiqixu05 <weiqixu2005@icloud.com>
2023-07-18 23:57:16 -0400

    added more collection formatting

--- src/frontend/pages/Collection/Collection.jsx
+++ src/frontend/pages/Collection/Collection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Collection.css";
 import "../../styles.css";
 import {recipeData} from "../../modules/recipes.js";
+import {isMobile, isTablet, isDesktop} from "react-device-detect";
 
 export default function Collection(){
     return(
@@ -9,7 +10,7 @@ export default function Collection(){
             <div className="head">
                 <h1>List of Recipes</h1>
             </div>
-            {
+            {/*isDesktop||*/isTablet?(
                 [...recipeData].map((recipe,i)=>{
                     if(i%2==0){
                         return(
@@ -17,16 +18,16 @@ export default function Collection(){
                             <div className="recipe-text">
                                 <p>{recipe.name}</p>
                             </div>
-                            <div className="recipe-image">
-                                <img src={recipe.img}/>
+                            <div className="recipe-image-box">
+                                <img className="recipe-image" src={recipe.img}/>
                             </div>
                         </div>
                         )
                     } else{
                         return(
                             <div className="recipe-body" key={i}>
-                                <div className="recipe-image">
-                                    <img src={recipe.img}/>
+                                <div className="recipe-image-box">
+                                    <img className="recipe-image" src={recipe.img}/>
                                 </div>
                                 <div className="recipe-text">
                                     <p>{recipe.name}</p>
@@ -36,6 +37,44 @@ export default function Collection(){
                         }
                     }
                 )
+            ):(
+                [...recipeData].map((recipe,i)=>{
+                        return(
+                        <div className="recipe-body-mobile" key={i}>
+                            <div className="recipe-text">
+                                <h2>{recipe.name}</h2>
+                                <p>{recipe.description}</p>
+                                <details>
+                                    <ol>
+                                        {[...recipe.ingredients].map((ingredient,j)=>{
+                                            return(<div key={j}><li>{ingredient}</li></div>)
+                                        })}
+                                    </ol>
+                                    <summary>Ingredients</summary>
+                                </details>
+                                <details>
+                                    {[...recipe.steps].map((step,j)=>{
+                                        return(
+                                            //have to fix this as no show
+                                            <div key={j}>
+                                                <details>
+                                                    {step}
+                                                    <summary>Step {j}</summary>
+                                                </details>
+                                            </div>
+                                        )
+                                    })}
+                                    <summary>Steps</summary>
+                                </details>
+                            </div>
+                            <div className="recipe-image-box">
+                                <img className="recipe-image" src={recipe.img}/>
+                            </div>
+                        </div>
+                        )
+                    }
+                )
+            )
             }
         </div>
     )
