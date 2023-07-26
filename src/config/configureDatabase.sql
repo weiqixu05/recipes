4562579583eb228036a5277d2f318cb9be8d1db7
weiqixu05 <weiqixu2005@icloud.com>
2023-07-25 23:06:53 -0400

    configure sql

---
+++ src/config/configureDatabase.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE recipe_db;
+
+create table recipe_db.recipes(
+    image_link      CHAR(200),
+    name            CHAR(100),
+    description     CHAR(500),
+    /*have to figure out the rest*/    
+);
