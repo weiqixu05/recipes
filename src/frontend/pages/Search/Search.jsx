68e3d53bb9c68f38d1bbd9fda02a2b5f110d77f3
weiqixu05 <weiqixu2005@icloud.com>
2023-08-22 22:36:23 -0400

    fixed infinite render

--- src/frontend/pages/Search/Search.jsx
+++ src/frontend/pages/Search/Search.jsx
@@ -3,32 +3,32 @@ import "./Search.css";
 import "../../styles.css";
 import {useState} from "react";
 
-
-var query="pizza";
-var hitNumber=0;
-var max=0;
-var count=0;
 export default function Search(){
+    const [query, setQuery]=useState("pizza");
     const [recipeLink, setRecipeLink]=useState("");
-    async function fetchData(query, hitNumber){
+    const [hitNumber, setHitNumber]=useState(3);
+    async function fetchData(query){
         try{
             const response=((await fetch("https://api.edamam.com/api/recipes/v2?type=public&q="+query+"&app_id=33eef802&app_key=8ebc6ae94cc66037076a9fd9c7e9c5fa")));
             const data=await response.json();
-            //console.log(hitNumber);
-            if((hitNumber>data.count-1)&&data.count!=0){
-                hitNumber=data.count-1;
-            } else if(data.count==0){
-                setRecipeLink("No Recipes Found");
-            }
-            else{setRecipeLink(data.hits[hitNumber].recipe.url);}
-            //console.log(hitNumber);
             //console.log(data.count);
+            if(data.count!=0&&data.count<20){
+                setHitNumber(Math.floor(Math.random()*data.count));
+                setRecipeLink(data.hits[hitNumber].recipe.url);
+            } else if(data.count!=0&data.count>=20){
+                setHitNumber(Math.floor(Math.random()*20));
+                console.log(hitNumber);
+                setRecipeLink(data.hits[hitNumber].recipe.url);
+            } else if(data.count==0){
+                setRecipeLink("No Recipes Found for\"" + query + "\"");
+            }
+            //console.log(hitNumber);
+            //console.log(query);
             //console.log(recipeLink);
         } catch (error){
             console.log(error);
         } 
     }
-    fetchData(query, hitNumber);
     return(
         <div>
             <div className="head">
@@ -38,21 +38,12 @@ export default function Search(){
                 <p>
                     {recipeLink}
                 </p>    
-                <button onClick={() =>{
-                        query="dumplings";
-                        fetchData(query, hitNumber);
-                    }}
-                >
-                    Change recipe
-                </button>
                 {/*update this part for user input using form*/}
                 <input name="search" id="search" type="text"/>
                 <button onClick={()=>{
-                        query=document.getElementById('search').value;
-                        max=20;
-                        hitNumber=Math.floor(Math.random()*max);
-                        //console.log(hitNumber);
-                        fetchData(query, hitNumber);
+                        setQuery(document.getElementById('search').value);
+                        //console.log(query);
+                        fetchData(query);
                     }}
                 >
                     Random Recipe
